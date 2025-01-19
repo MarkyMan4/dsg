58751193d9716aa5a16761d9fd00bbc59086ffca
MarkyMan4 <markus.hutnik@gmail.com>
2025-01-18 23:43:09 -0600

    handling CLI args

--- src/dsg/__init__.py
+++ src/dsg/__init__.py
@@ -1,2 +1,21 @@
+import argparse
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest="subcommand")
+    init_parser = subparsers.add_parser("init", help="initialize a new project")
+    build_parser = subparsers.add_parser("build", help="build project as a static site")
+    init_parser.add_argument("project_name")
+    build_parser.add_argument("test")
+
+    args = parser.parse_args()
+
+    return args
+    
+
 def main() -> None:
-    print("Hello from dsg!")
+    args = parse_arguments()
+    
+    if args.subcommand == "init":
+        print("initialize new project")
