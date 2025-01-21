f4d688cc0b1f3ef7072046ffa1efc221a66c3cb3
MarkyMan4 <markus.hutnik@gmail.com>
2025-01-20 21:15:39 -0600

    POC plotly chart via custom jinja function

--- src/dsg/render.py
+++ src/dsg/render.py
@@ -2,14 +2,24 @@ from os.path import dirname, join
 from pathlib import Path
 
 import markdown
+import plotly.express as px
 from jinja2 import Environment, FileSystemLoader
 
 TEMPLATE_DIR = join(dirname(__file__), "templates")
 
 
+def bar_chart(x, y) -> str:
+    fig = px.bar(x=x, y=y)
+    return fig.to_html()
+
+
 def render_pages():
     # read markdown files, render jinja, convert to HTML, then write to dist folder
     env = Environment(loader=FileSystemLoader(["pages", TEMPLATE_DIR]))
+
+    # register custom jinja functions
+    env.globals["bar_chart"] = bar_chart
+
     templ = env.get_template("index.md")
     content = markdown.markdown(templ.render())
 
--- tests/sample_proj/pages/index.md
+++ tests/sample_proj/pages/index.md
@@ -1,3 +1,5 @@
 # My DSG Project
 
 Hello data!
+
+{{ bar_chart(x=["a", "b", "c"], y=[1, 2, 3]) }}
