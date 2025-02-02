86d313c73845ec37c9dfe80d8b13d011ec6ce4f6
MarkyMan4 <markus.hutnik@gmail.com>
2025-02-02 14:47:03 -0600

    added scatter charts

--- src/dsg/jinja_functions.py
+++ src/dsg/jinja_functions.py
@@ -1,19 +1,44 @@
+from typing import Any
+
 import plotly.express as px
 import polars as pl
 from jinja2 import Environment
 
+# TODO allow users to define their own custom functions in dsg.yml
+#      when parsing config, import from their file and load those functions to the environment
+#      the only requirement for custom functions is that it returns a string to render on the page
+
 
 def bar_chart(
-    data: pl.DataFrame, x: str, y: str, color: str = None, title: str = None
+    data: pl.DataFrame = None,
+    x: Any = None,
+    y: Any = None,
+    color: str = None,
+    title: str = None,
 ) -> str:
-    fig = px.bar(data, x=x, y=y, color=color, title=title)
+    fig = px.bar(data_frame=data, x=x, y=y, color=color, title=title)
     return fig.to_html()
 
 
 def line_chart(
-    data: pl.DataFrame, x: str, y: str, color: str = None, title: str = None
+    data: pl.DataFrame = None,
+    x: Any = None,
+    y: Any = None,
+    color: str = None,
+    title: str = None,
 ) -> str:
-    fig = px.line(data, x=x, y=y, color=color, title=title)
+    fig = px.line(data_frame=data, x=x, y=y, color=color, title=title)
+    return fig.to_html()
+
+
+def scatter_chart(
+    data: pl.DataFrame = None,
+    x: Any = None,
+    y: Any = None,
+    color: str = None,
+    title: str = None,
+) -> str:
+    fig = px.scatter(data_frame=data, x=x, y=y, color=color, title=title)
     return fig.to_html()
 
 
@@ -21,3 +46,4 @@ def register_functions(env: Environment):
     # register the functions in this file as functions in the given environment
     env.globals[bar_chart.__name__] = bar_chart
     env.globals[line_chart.__name__] = line_chart
+    env.globals[scatter_chart.__name__] = scatter_chart
--- tests/sample_proj/pages/index.md
+++ tests/sample_proj/pages/index.md
@@ -3,5 +3,5 @@
 Hello data!
 
 {{ bar_chart(all_data, 'x', 'y') }}
-
 {{ line_chart(all_data, 'x', 'y') }}
+{{ scatter_chart(all_data, 'x', 'y') }}
