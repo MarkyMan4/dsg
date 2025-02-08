b474d81e0f0a004b6955b4eaf76ac26897c73660
MarkyMan4 <markus.hutnik@gmail.com>
2025-02-08 00:01:22 -0600

    added clean command

--- src/dsg/__init__.py
+++ src/dsg/__init__.py
@@ -1,4 +1,5 @@
 import argparse
+import shutil
 
 from .core import initialize_project, load_config, render_pages
 
@@ -9,6 +10,7 @@ def parse_arguments():
     init_parser = subparsers.add_parser("init", help="initialize a new project")
     subparsers.add_parser("build", help="build project as a static site")
     init_parser.add_argument("project_name")
+    subparsers.add_parser("clean", help="remove build artifacts")
 
     args = parser.parse_args()
 
@@ -30,3 +32,10 @@ def main() -> None:
         config = load_config()
         render_pages(config)
         print("Build complete! The build is available in the dist folder")
+
+    elif args.subcommand == "clean":
+        try:
+            print("removing dist")
+            shutil.rmtree("dist")
+        except:
+            pass
