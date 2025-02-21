83652f119d93b7e3683291dcebf2512b14ddd85b
MarkyMan4 <markus.hutnik@gmail.com>
2025-02-21 16:11:03 -0600

    docstrings

--- src/dsg/builder.py
+++ src/dsg/builder.py
@@ -47,7 +47,9 @@ class SiteBuilder:
         read markdown files, render jinja, convert to HTML, then write to dist folder
         """
 
-        # TODO disallow index.md in pages directory
+        # Load queries in DataFrames and parse pages/convert to HTML. Pages are 
+        # converted right away because I need to read them to get the metadata from them 
+        # (e.g. page title). This metadata is needed later when writing the final HTML file
         context = self._read_queries(self.config.connection)
         pages = self._read_pages(context)
         page_links = {page.title: page.link for page in pages}
@@ -62,7 +64,9 @@ class SiteBuilder:
             self._render_page(page=page, target_path=Path(PAGES_DIR), links=page_links)
 
     def _read_pages(self, context: dict[str, pl.DataFrame]) -> list[Page]:
-        # parse each page in the pages directory into a Page object
+        """
+        Parse each page in the pages directory into a Page object
+        """
         pages = []
         pages_path = Path(PAGES_DIR)
 
@@ -78,7 +82,10 @@ class SiteBuilder:
     def _parse_page(
         self, page: Path, context: dict[str, pl.DataFrame], is_index=False
     ) -> Page:
-        # given a page, render the markdown template, convert to HTML and create Page object with title, link and content
+        """
+        Given a page, render the markdown template, convert to HTML and create Page 
+        object with title, link and content
+        """
         md = markdown.Markdown(extensions=["meta"])
         md_templ = self.env.get_template(str(page))
         html = md.convert(md_templ.render(**context))
@@ -97,7 +104,9 @@ class SiteBuilder:
         return Page(file_stem=file_stem, title=title, link=link, content=html)
 
     def _render_page(self, page: Page, target_path: Path, links: dict[str, str]):
-        # fille in the page.html template with page content and write to dist folder
+        """
+        Fill in the page.html template with page content and write to dist folder
+        """
         html_templ = self.env.get_template(PAGE_TEMPLATE_FILE)
         page_html = html_templ.render(
             title=page.title, content=page.content, pages=links
@@ -110,8 +119,10 @@ class SiteBuilder:
             outfile.write(page_html)
 
     def _read_queries(self, conn_info: ConnectionInfo) -> dict[str, pl.DataFrame]:
-        # read queries from sql directory into dictionary where key is file name (without extension)
-        # and value is a polars dataframe with the query result
+        """
+        Read queries from sql directory into dictionary where key is file name (without extension)
+        and value is a polars dataframe with the query result
+        """
         conn = get_connection(conn_info)
         query_results = {}
 
